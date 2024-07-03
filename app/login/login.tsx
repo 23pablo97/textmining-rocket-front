73e81186f5e01ecec142d189536f45ac2adfe6a9
Pablo Arancibia Barahona <23pablo97@gmail.com>
2024-07-03 16:22:25 +0200

    ADD handler of error in login

--- app/login/login.tsx
+++ app/login/login.tsx
@@ -2,19 +2,24 @@
 import { useForm } from "react-hook-form";
 import { useRouter } from 'next/navigation';
 import { login } from '@/utils/api';
+import { useState } from "react";
 
 export default function Login() {
     const router = useRouter();
     const { handleSubmit, register, formState: { errors } } = useForm();
+    const [ message, setMessage ] = useState("");
     
     const onSubmit = async (values: any) =>{
         console.log(values);
         try {
             const response = await login(values.username, values.password);
+            console.log(response);
             if (response){
+                setMessage("");
                 router.push('/auth/resources');
+            } else {
+                setMessage("Username or password incorrect.");
             }
-            // TODO: ADD CONTROL OF ERROR
         } catch (error) {
             console.error('Error logging in:', error);
         }
@@ -40,6 +45,19 @@ export default function Login() {
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    { message !== "" ? 
+                        (<div className="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                            <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                            </svg>
+                            <span className="sr-only">Info</span>
+                            <div>
+                                {message}
+                            </div>
+                        </div>)
+                    :
+                        <></>
+                    }
                     <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
                         <div>
                             <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
